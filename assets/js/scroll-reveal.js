67da4e62a3305418368963ab488a0d0e655027b6
Pedro Augusto <p.augusto_23@outlook.com>
2022-12-02 18:51:06 -0300

    :tada: Commit inicial

---
+++ assets/js/scroll-reveal.js
@@ -0,0 +1,164 @@
+ScrollReveal().reveal('.logo', {
+  delay: 300,
+  duration: 1600,
+  origin: 'top',
+  distance: '60%',
+  easing: 'ease-out'
+})
+
+ScrollReveal().reveal('.search-bar', {
+  delay: 350,
+  duration: 1600,
+  origin: 'top',
+  distance: '60%',
+  easing: 'ease-out'
+})
+
+ScrollReveal().reveal('.nav-button', {
+  delay: 400,
+  duration: 1600,
+  origin: 'top',
+  distance: '60%',
+  easing: 'ease-out'
+})
+
+ScrollReveal().reveal('.hero-content', {
+  delay: 600,
+  duration: 1600,
+  origin: 'top',
+  distance: '60%',
+  easing: 'ease-out'
+})
+ScrollReveal().reveal('.hero-title', {
+  delay: 650,
+  duration: 1000,
+  origin: 'top',
+  distance: '90%'
+})
+
+ScrollReveal().reveal('.hero-subtitle', {
+  delay: 700,
+  duration: 1000,
+  origin: 'top',
+  distance: '90%'
+})
+
+ScrollReveal().reveal('.button-down', {
+  delay: 800,
+  duration: 1000,
+  origin: 'top',
+  distance: '90%'
+})
+
+ScrollReveal().reveal('.first-down', {
+  delay: 1150,
+  duration: 1300,
+  origin: 'top',
+  distance: '90%'
+})
+
+ScrollReveal().reveal('.second-down', {
+  delay: 1250,
+  duration: 1300,
+  origin: 'top',
+  distance: '90%'
+})
+
+ScrollReveal().reveal('.last-down', {
+  delay: 1350,
+  duration: 1300,
+  origin: 'top',
+  distance: '90%'
+})
+
+ScrollReveal().reveal('.hero-image', {
+  delay: 1200,
+  duration: 1600,
+  origin: 'top',
+  distance: '30%',
+  easing: 'ease-out',
+  cleanup: true
+})
+
+ScrollReveal().reveal('.first-image', {
+  delay: 1900,
+  duration: 1800,
+  origin: 'top',
+  distance: '40%',
+  cleanup: true
+})
+
+ScrollReveal().reveal('.second-image', {
+  delay: 1950,
+  duration: 1800,
+  origin: 'top',
+  distance: '40%',
+  cleanup: true
+})
+
+ScrollReveal().reveal('.last-image', {
+  delay: 2050,
+  duration: 1800,
+  origin: 'top',
+  distance: '40%',
+  cleanup: true
+})
+
+ScrollReveal().reveal('.collections-subtitle', {
+  delay: 2000,
+  duration: 1800,
+  origin: 'top',
+  distance: '40%'
+})
+
+ScrollReveal().reveal('.collection-button', {
+  delay: 400,
+  duration: 1800,
+  origin: 'top',
+  distance: '40%'
+})
+
+ScrollReveal().reveal('.artists-logo', {
+  delay: 600,
+  duration: 1800,
+  origin: 'top',
+  distance: '40%'
+})
+
+ScrollReveal().reveal('.first-card', {
+  delay: 1100,
+  duration: 2500,
+  origin: 'top',
+  distance: '20%',
+  cleanup: true
+})
+
+ScrollReveal().reveal('.second-card', {
+  delay: 1200,
+  duration: 2500,
+  origin: 'top',
+  distance: '30%',
+  cleanup: true
+})
+
+ScrollReveal().reveal('.last-card', {
+  delay: 1300,
+  duration: 2500,
+  origin: 'top',
+  distance: '40%',
+  cleanup: true
+})
+
+ScrollReveal().reveal('.footer-logo', {
+  delay: 1300,
+  duration: 2500,
+  origin: 'top',
+  distance: '60%'
+})
+
+ScrollReveal().reveal('.footer-nav', {
+  delay: 1400,
+  duration: 2000,
+  origin: 'top',
+  distance: '60%'
+})
